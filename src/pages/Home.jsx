5059caa4034b4eadb8035bc353ec6b6858bd1a57
Youkwhan <37788922+Youkwhan@users.noreply.github.com>
2022-08-03 00:20:00 -0400

    getting Products from the firebase db

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,11 @@ import React, { useState, useEffect } from "react";
 import Navbar from "../components/Navbar";
 import Products from "../components/Products";
 import { auth, db } from "../config/firebase-config";
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDoc, collection, getDocs } from "firebase/firestore";
 
-//Navbar and Products components are rendered inside a parent component Homepage
+// Navbar and Products components are rendered inside a parent component Homepage
 function Home() {
-	//get curr user Function
+	// get curr user Function
 	const GetCurrentUser = () => {
 		const [user, setUser] = useState(null);
 		useEffect(() => {
@@ -25,13 +25,37 @@ function Home() {
 	};
 
 	const user = GetCurrentUser();
-	//console.log(user);
+	// console.log(user);
+
+	// state of products
+	const [products, setProducts] = useState([]);
+
+	// getting Products Function
+	const getProducts = async () => {
+		//Get all products from db => collection => Products folder
+		const products = await getDocs(collection(db, "Products"));
+		const productsArray = [];
+		for (var snap of products.docs) {
+			var data = snap.data();
+			data.ID = snap.id;
+			productsArray.push({
+				...data,
+			});
+			if (productsArray.length === products.docs.length) {
+				setProducts(productsArray);
+			}
+		}
+	};
+
+	useEffect(() => {
+		getProducts();
+	}, []);
 
 	return (
 		<>
 			{/* Props are arguments passed into React components */}
-			<Navbar user={user}/>
-			<Products />
+			<Navbar user={user} />
+			<br></br>
 		</>
 	);
 }
